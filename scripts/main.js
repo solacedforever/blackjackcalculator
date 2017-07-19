5c80ab44c36affedf2359e986442f38fd2c7e97e
James Spargo <solacedforever@gmail.com>
2017-07-19 14:19:16 -0400

    changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,21 @@
+//  Implement a Blackjack hand value calculator.
 
-  //  Implement a Blackjack hand value calculator.
-
-function handValue (hand) {
-    let total = 0
-    for ( i = 0 ; i < hand.length; i ++) {
+function handValue(hand) {
+  let total = 0
+  for (i = 0; i < hand.length; i++) {
     let card = hand[i]
-    if (card ==="K" || card === "Q" || card === "J"){
+    if (card === "K" || card === "Q" || card === "J") {
       total += 10;
- } else if (card === "A" && total >= 11) {
-   total += 1;
- } else if (card === "A" && total <= 11) {
-   total += 11;
- } else {
-   total += Number(card);
- }
- if (hand.includes("A") && total > 21) {
-   total -= 10;
- }
-}
-return total;
+    } else if (card === "A" && total >= 11) {
+      total += 1;
+    } else if (card === "A" && total <= 11) {
+      total += 11;
+    } else {
+      total += Number(card);
+    }
+    if (hand.includes("A") && total > 21) {
+      total -= 10;
+    }
+  }
+  return total;
 }
